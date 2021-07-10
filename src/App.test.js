590147f0440ef571098ab83356f7262ebf18b217
Diego Acosta <diegoacosta211@gmail.com>
2021-07-10 14:22:47 -0300

    some failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Giffy/i);
+  expect(title).toBeInTheDocument();
+});
+
+test('search form could be used', async () => {
+  render(<App />);
+  const textBox = await screen.findByRole('textbox');
+  const button = await screen.findByRole('button');
+  fireEvent.change(textBox, { target: { value: 'Matrix' }} );
+  fireEvent.click(button)
+
+  const title = await screen.findByText('Matrix');
+  expect(title).toBeVisible();
 });
