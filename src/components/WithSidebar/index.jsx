548195a133483771cd264347d9367e3826648d2d
Diego Acosta <diegoacosta211@gmail.com>
2021-07-11 20:30:38 -0300

    changes in tailwind config

--- src/components/WithSidebar/index.jsx
+++ src/components/WithSidebar/index.jsx
@@ -4,10 +4,10 @@ export default function WithSidebar(Component) {
   return function WithSidebarComponent({...props}) {
     return (
       <div className="grid grid-cols-12 gap-6">
-        <div className="col-span-12 lg:col-span-9 min-h-screen">
+        <div className="col-span-12 md:col-span-9 min-h-screen">
           <Component {...props} />
         </div>
-        <div className="col-span-12 lg:col-span-3 mt-10 md:mt-3">
+        <div className="col-span-12 md:col-span-3 mt-10 md:mt-3">
           <LazyCategories />
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'class', // or 'media' or 'class'
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     container: {
       padding: '1rem',
-      // screens: {
-      //    sm: "100%",
-      //    md: "100%",
-      //    lg: "1024px",
-      //    xl: "1200px"
-      // }
+      screens: {
+         sm: "100%",
+         md: "100%",
+         lg: "1024px",
+         xl: "1200px"
+      }
     },
   },
   variants: {
